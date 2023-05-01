af76740f60bf4c342e9e900c6a7f99cd8f313057
JWin23 <iamkaungthantwin@gmail.com>
2023-05-01 17:50:32 -0500

    added database

---
+++ database/createtable.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS baseball;
+CREATE TABLE baseball (
+  team text,
+  winpercent real,
+  catchers real,
+  infielders real,
+  outfielders real,
+  dh real,
+  pitchers real,
+  totalpayroll real,
+  payyear int,
+  roundmade int,
+);
